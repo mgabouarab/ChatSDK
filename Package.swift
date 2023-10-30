bfebf67d9b46b8b50397740c968c4bb13a02a950
mgabouarab <mgabouarab@gmail.com>
2023-10-30 17:48:06 +0200

    version 0.0.1

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ChatSDK",
+    platforms: [
+      .macOS(.v12), .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,12 +15,12 @@ let package = Package(
             targets: ["ChatSDK"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "ChatSDK"),
-        .testTarget(
-            name: "ChatSDKTests",
-            dependencies: ["ChatSDK"]),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "ChatSDK",
+            path: "./Sources/ChatSDK.xcframework")
     ]
 )
+
+
