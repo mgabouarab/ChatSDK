4f4ae1ba92f943418ec21fc5543e239eacc382e8
mgabouarab <mgabouarab@gmail.com>
2023-10-30 17:47:30 +0200

    version 0.0.1

---
+++ Sources/ChatSDK.xcframework/ios-arm64_x86_64-simulator/ChatSDK.framework/Headers/ChatSDK.h
@@ -0,0 +1,18 @@
+//
+//  ChatSDK.h
+//  ChatSDK
+//
+//  Created by MGAbouarab on 30/10/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatSDK.
+FOUNDATION_EXPORT double ChatSDKVersionNumber;
+
+//! Project version string for ChatSDK.
+FOUNDATION_EXPORT const unsigned char ChatSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatSDK/PublicHeader.h>
+
+
