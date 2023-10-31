5e0aafe310e573e532d650ade1297f13f47158a4
mgabouarab <mgabouarab@gmail.com>
2023-10-31 18:33:49 +0200

    version 0.0.9

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // swift-tools-version: 5.9.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+//// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
